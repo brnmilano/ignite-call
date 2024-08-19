134e9a91c54fadbfc75660520225bc2117c85941
Bruno <brn.milano@hotmail.com>
2024-08-19 20:12:33 -0300

    Setup & Estrutura: - Criação do projeto em Next; - Instalação do Design System; - Configuração do ESLint; - Configuraçaõ do PageExtensions;

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ["page.tsx", "api.ts", "api.tsx"],
 };
 
 export default nextConfig;
+
+/**
+ * Para informar ao Next.js quais as extensões de arquivos queremos que ele
+ * transforme em rotas/páginas da aplicação.
+ *
+ * Arquivo: next.config.js pageExtensions: ['page.tsx', 'api.ts', 'api.tsx']
+ *
+ * A partir de agora, todo arquivo que deverá ser uma página, precisa terminar
+ * com page.tsx, inclusive o _app e o _document.
+ *
+ * Por exemplo: _app.page.tsx, _document.page.tsx, home.page.tsx, etc.
+ */
